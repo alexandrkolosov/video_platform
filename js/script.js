4f5548dbac1a84c111f035a46175b9614e9ed818
alexanderkolosov <avk7961555@gmail.com>
2024-09-18 15:38:02 +0300

    search is added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const global = {
         type: '',
         page: 1,
         totalPages: 1
+    },
+    api: {
+        apiKey: '17287986897715e01bf612f98567e799',
+        apiUrl: 'https://api.themoviedb.org/3/'
     }
 };
 
@@ -238,6 +242,17 @@ async function search() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
+    global.search.type = urlParams.get('type');
+    global.search.term = urlParams.get('search-term');
+
+    if (global.search.term !== '' && global.search.term !== null) {
+        const results = await searchAPIData();
+        console.log(results);
+
+    } else {
+        showAlert('Please put a request')
+    }
+
 }
 
 // Display Slider Movies
@@ -290,8 +305,8 @@ function initSwiper() {
 //Fetch data from TMDB API
 
 async function fetchAPIData(endpoint) {
-    const API_KEY = '17287986897715e01bf612f98567e799';
-    const API_URL = 'https://api.themoviedb.org/3/';
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl;
 
     showSpinner();
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
@@ -300,6 +315,33 @@ async function fetchAPIData(endpoint) {
     hideSpinner();
 
     return data;
+}
+
+
+//Make Request to search
+
+async function searchAPIData() {
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl;
+
+    showSpinner();
+    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+    const data = await response.json();
+
+    hideSpinner();
+
+    return data;
+}
+
+// Show customise Alert
+
+function showAlert(message, className) {
+    const alertEl = document.createElement('div');
+    alertEl.classList.add('alert', className);
+    alertEl.appendChild(document.createTextNode(message));
+    document.querySelector('#alert').appendChild(alertEl);
+
+    setTimeout(() => alertEl.remove(), 4000);
 }
 
 // function to $
@@ -327,7 +369,7 @@ function init() {
             displayShowsDetails()
             break;
         case '/search.html':
-            console.log('Search')
+            search();
             break;
     }
     highlightActiveLink();
